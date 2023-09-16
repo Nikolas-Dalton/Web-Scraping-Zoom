617b34cb8a9973f7ca9cac44d7b4f3019f73abf9
Nikolas Dalton Perassoli Varella <71299389+Nikolas-Dalton@users.noreply.github.com>
2023-09-16 18:13:34 -0300

    adi;áo dos pre;os / come;o da busca
    adi;;áo de pre;os da tela e o come;o das buscas pelas paginas

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ response= requests.get(url_base + "search?q=" + produto_name)
 
 site = BeautifulSoup(response.text, "html.parser")
 
-produto = site.find("a", attrs={"class": "ProductCard_ProductCard_Inner__tsD4M"})
+produtos = site.findAll("a", attrs={"class": "ProductCard_ProductCard_Inner__tsD4M"})
 
-titulo = produto.find("h2",attrs={"class":"Text_Text__h_AF6 Text_MobileLabelXs__ER_cD Text_DesktopLabelSAtLarge__baj_g ProductCard_ProductCard_Name__LT7hv"} )
+for produto in produtos:
+    titulo = produto.find("h2",attrs={"class":"Text_Text__h_AF6 Text_MobileLabelXs__ER_cD Text_DesktopLabelSAtLarge__baj_g ProductCard_ProductCard_Name__LT7hv"} )
+    # link = produto.find("a",attrs={"class":"ProductCard_ProductCard_Inner__tsD4M"})
 
-# link = produto.find("a",attrs={"class":"ProductCard_ProductCard_Inner__tsD4M"})
+    real = produto.find("p" ,attrs={"class":"Text_Text__h_AF6 Text_MobileHeadingS__Zxam2"})
 
-real = produto.find("p" ,attrs={"class":"Text_Text__h_AF6 Text_MobileHeadingS__Zxam2"})
-centavo = produto.find()
-print(produto.prettify())
-print("Titulo do produto: ",titulo.text)
-# print("link do produto: ",link["href"])
+
+    # print(produto.prettify())
+    print("Titulo do produto: ",titulo.text)
+    print("preço do produto: R$",real.text)
+#    print("link do produto: ",link["href"])
+    print("\n\n")
